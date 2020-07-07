ddc2cc18ed6d5e4020da1efe7144ec220df1fb5b
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-07-07 22:17:22 +0530

    if-else

---
+++ conditional.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int hour;
+	cout<<"enter hour";
+	cin>>hour;
+	if(hour>=9 && hour<=18)
+	{
+		cout<<"working"<<endl;
+	}
+	else{
+		cout<<"leisure";
+	}
+	return 0;
+}
