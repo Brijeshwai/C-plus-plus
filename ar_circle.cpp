d9b109dbb55d9b7399706318d49f50719b790e27
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-07-08 16:25:29 +0530

    area of circle

---
+++ ar_circle.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	int r;
+	float a;
+
+	cout<<"enter a radius::";
+	cin>>r;
+
+	a=3.14*r*r;
+	cout<<"area of circle  "<<a<<endl;
+	return 0;
+}
