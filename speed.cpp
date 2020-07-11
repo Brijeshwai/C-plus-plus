5d453d1fd7cfd72c9ff8fcf5e0e5e66972d0ff1e
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-07-11 22:06:16 +0530

    speed

---
+++ speed.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int u,v,a;
+	float speed;
+	cout<<enter 3 number;
+	cin>>u>>v>>a;
+	speed=(v*v-u*u)/(2*a);
+	cout<<speed<<endl;
+	return 0;
+}
