48615e83bb133713745f36e3b84b1bf1a62c1296
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-07-14 13:57:39 +0530

    Logical
    Logical operator

---
+++ logical.cpp
@@ -0,0 +1,16 @@
+ #include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	int x,y,z;
+	cout<<"enter 3 number";
+	cin>>x>>y>>z;
+	if(x>y && x>z)
+	
+		cout<<"x is greater"<<endl;
+	
+		return 0;
+}
