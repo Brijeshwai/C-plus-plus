abb4d76f8b315f5d1aadbb86c479ee46deaa69c3
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-07-14 13:55:54 +0530

    Grade
    Grade of the student

---
+++ grade.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+int main(){
+	int m1,m2,m3,total;
+	float average;
+	cout<<"enter the marks";
+	cin>>m1>>m2>>m3;
+	total=m1+m2+m3;
+	average=total/3;
+
+	if(average>=60)
+		cout<<"A"<<endl;
+
+	else if(average>=35)
+		cout<<"B"<<endl;
+
+		else
+			cout<<"C"<<endl;
+		return 0;
+	
+}
