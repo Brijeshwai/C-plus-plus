ea6d1993e3ba72d18bc8422d1e13655c35886590
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-07-23 22:47:47 +0530

    Distance
    Distance between two points.

---
+++ distance_btw_points.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int x1,x2,y1,y2;
+	float dist;
+	cout<<"enter the points";
+	cin>>x1>>x2>>y1>>y2;
+	dist=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		cout<<"Distance between two points are::"<<endl<<dist<<endl;
+	return 0;	
+}
