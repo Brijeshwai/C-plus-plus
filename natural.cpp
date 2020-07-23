faab3da0904113034b2a95446a2a0bdce1adae75
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-07-23 22:46:21 +0530

    natural
    sum of natural numbers

---
+++ natural.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,sum;
+	cout<<"enter number::";
+	cin>>n;
+	sum=n*(n+1)/2;
+	cout<<"Sum of natural number is:-"<<endl<<sum<<endl;
+	return 0;
+}
