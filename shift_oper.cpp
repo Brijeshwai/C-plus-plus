6baa1664d1925be28f1d8e66ba6ec57819e4a741
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-08-03 07:03:46 +0530

    Shift operator

---
+++ shift_oper.cpp
@@ -0,0 +1,12 @@
+//left shift(<<) operator and right shift(>>) operator
+#include<iostream>
+#include<math.h>
+int main(int argc, char const *argv[])
+{
+	char x=20,y;
+	y=x>>1;//using right shift it will divide the value by 2.
+	//y=x<<1; using left shift it will multiply the value by 2(i).
+	//y=~x; using of not operator.
+	cout<<(int)y<<endl;
+	return 0;
+}
