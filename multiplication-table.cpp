5da14e68d8be2186791c91a6fd363037ad094164
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-08-20 19:07:23 +0530

    table

---
+++ multiplication-table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i,a;
+	cout<<"enter a number:-";
+	cin>>n;
+	for(int i=1;i>=10;i++){
+		cout<<n<<"x"<<i<<"="<<i*n;
+		cout<<endl;
+	}
+	return 0;
+}
