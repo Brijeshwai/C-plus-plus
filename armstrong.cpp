13a555774fb8fd00242cbf5c171c4787395907a8
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-09-22 14:03:40 +0530

    armstrong number

---
+++ armstrong.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+ 
+using namespace std;
+
+ int main()
+ {
+   
+  int m,n,sum=0;
+     
+  
+   cout<<"enter a number:-";
+   
+
+  cin>>n;
+    
+ m=n;
+     
+while(n>0)
+   
+  {
+        
+ m=n%10;
+     
+    n=n/10;
+    
+     sum=sum+m*m*m;
+  
+   }
+   
+  if(sum==m)
+{
+       
+  cout<<"armstrong number"<<endl;
+  
+   }
+ 
+    else
+     cout<<"not armstong number"<<endl;
+  
+   return 0;
+ }
