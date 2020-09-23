40392e4283913eccf5cf25ad155760e7d5b6602b
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-09-23 20:39:00 +0530

    GDC number

---
+++ gdc.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+   
+ int m,n;
+
+    cout<<"enter two number:-";
+ 
+   cin>>n>>m;
+   
+ while(m!=n){
+ 
+         if(m>n)
+  
+     m=m-n;
+  
+      else if(n>m);
+   
+  n=n-m;
+  
+  }
+  
+  cout<<m<<" "<<endl;
+  
+  return 0;
+
+}
