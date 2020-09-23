f4ffc55e55595ebf2a6fe5f04245a9fd1c5f90cf
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-09-23 20:37:59 +0530

    reverse number

---
+++ reverseno.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+  
+  int m,n, rev=0;
+    
+cout<<"enter a number:-";
+ 
+   cin>>n;
+   
+ m=n;
+ 
+   while(n>0){
+     
+   m=n%10;
+    
+    n=n/10;
+     
+   rev=rev*10+m;
+  
+  }
+   
+ cout<<rev<<endl;
+   
+ return 0;
+
+}
