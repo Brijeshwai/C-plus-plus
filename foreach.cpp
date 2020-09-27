2cdf347ed2014f4b1719ac2dc8c3f44327cd83b7
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-09-27 08:47:34 +0530

    for each loop

---
+++ foreach.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A[]={2,4,6,9,11,13};
+	for (int x:A)
+		//for(int &x:A)
+		//cout<<++x<<" ";
+		//& this is called refrence it will change the storage increasing by one...
+	cout<<x<<" ";
+	return 0;
+}
