ef9f7963bef305578692e5239f37a57675cf89ea
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-10-12 08:47:24 +0530

    Update Operators.cpp

--- Operators.cpp
+++ Operators.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// Complete the solve function below.
+
 void solve(double meal_cost, int tip_percent, int tax_percent) {
 double tip=meal_cost*tip_percent/100;
     double tax=meal_cost*tax_percent/100;
