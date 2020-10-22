84b50a08ff814f44d26f1a1dd561f97d3ec7d6fa
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-10-22 12:49:19 +0530

    Sum of Array

---
+++ sumofarray.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main(){
+    int A[10]={2,4,6,9,7,5,3,2,55,67};
+    int sum=0;
+    for(int i=0;i<10;i++)
+    sum=sum+A[i];
+cout<<"sum of all elements:-"<<sum<<endl;
+
+    return 0;
+}
