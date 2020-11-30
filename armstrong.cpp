bb7a9231ec54a07161c15d7e601672f51fa8068e
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-11-30 12:59:19 +0530

    Update armstrong.cpp

--- armstrong.cpp
+++ armstrong.cpp
@@ -1,41 +1,41 @@
-#include<iostream>
-#include<math.h>
- 
-using namespace std;
+ #include<iostream>
+ #include<math.h>
 
- int main()
- {
-   
-  int m,n,sum=0;
-     
-  
-   cout<<"enter a number:-";   
+ using namespace std;
 
-  cin>>n;
-    
- m=n;
-     
-while(n>0)
-   
+  int main()
   {
-        
- m=n%10;
-     
-    n=n/10;
-    
-     sum=sum+m*m*m;
-  
-   }
-   
-  if(sum==m)
-{
-       
-  cout<<"armstrong number"<<endl;
-  
-   }
- 
-    else
-     cout<<"not armstong number"<<endl;
-  
-   return 0;
- }
+
+   int m,n,sum=0;
+
+
+    cout<<"enter a number:-";   
+
+   cin>>n;
+
+  m=n;
+
+ while(n>0)
+
+   {
+
+  m=n%10;
+
+     n=n/10;
+
+      sum=sum+m*m*m;
+
+    }
+
+   if(sum==m)
+ {
+
+   cout<<"armstrong number"<<endl;
+
+    }
+
+     else
+      cout<<"not armstong number"<<endl;
+
+    return 0;
+  }
