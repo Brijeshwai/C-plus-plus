d4f6efc77140fc338227b90eae793ab54de29f3a
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-12-08 18:43:44 +0530

    string character

---
+++ string-char.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+int main()
+{
+    //reads from the right side 
+     char s1[100]="Programming";
+     
+     cout<<strchr(s1,'m')<<endl;
+     
+    return 0;
+}
