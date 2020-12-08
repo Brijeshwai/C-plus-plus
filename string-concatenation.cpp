988347389551e89e0372844da6d779578394bfae
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-12-08 18:43:14 +0530

    string concatenation

---
+++ string-concatenation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+int main()
+{
+     char s1[100]="good ";
+     char s2[100]="morning";
+     
+     strcat(s1,s2);
+     
+     cout<<s1<<endl;
+    
+    return 0;
+}
