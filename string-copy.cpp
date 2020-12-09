f06c165c5f23acfca766cbd6b80210821414fce7
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-12-09 12:28:06 +0530

    string copy

---
+++ string-copy.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+int main()
+{
+     char s1[100]="good";
+     char s2[100]="";
+     
+     strcpy(s2,s1);
+     
+     cout<<s2<<endl;
+    
+    return 0;
+}
