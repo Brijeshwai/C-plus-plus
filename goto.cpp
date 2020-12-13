0aeca94fc3aa45d2e7452f1a52a15bdcd6ecd529
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-12-13 16:36:59 +0530

    Update goto.cpp

--- goto.cpp
+++ goto.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main ()
-{
-  int n=10;
-mylabel:
-  cout << n << ", ";
-  n--;
-  if (n>0) goto mylabel;
-  cout << "liftoff!\n";
-}
+  {
+    int n=10;
+    mylabel:
+    cout << n << ", ";
+    n--;
+    if (n>0) goto mylabel;
+    cout << "liftoff!\n";
+  }
