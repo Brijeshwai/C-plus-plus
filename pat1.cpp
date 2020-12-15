ca3e3e5ece4504b734714e6ff4c906fe8b3d302d
Brijeshwai <65451400+Brijeshwai@users.noreply.github.com>
2020-12-15 10:44:32 +0530

    Update pat1.cpp

--- pat1.cpp
+++ pat1.cpp
@@ -1,6 +1,8 @@
+#include<iostream>
 #include<stdio.h>
 
 using namespace std;
+
 int main()
 {
 	int r,c,i,j;
@@ -8,8 +10,9 @@ int main()
 	{
 		for(c=1;c<=r;c++)
 		{
-			printf("* ");
+			cout<<"* ";
 	    }
-			printf("\n");
+			cout<<"\n";
 	}
+	return 0;
 }
